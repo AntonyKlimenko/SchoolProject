2ecd2383c9561f39306c4b393166026f7ab21ec2
Anton Klimenko <antonyklimenko@gmail.com>
2022-06-28 20:50:21 +0300

    New test and optimisation

--- lesson21/SchoolProject/src/by/itstep/klimenko/lesson22/model/logic/SchoolManager.java
+++ lesson21/SchoolProject/src/by/itstep/klimenko/lesson22/model/logic/SchoolManager.java
@@ -3,20 +3,60 @@ package by.itstep.klimenko.lesson22.model.logic;
 public class SchoolManager {
     public static double CalculateAvgMark(int[][] groups) {
         double sum = 0;
+        int count = 0;
 
         for (int i = 0; i < groups.length; i++) {
-            sum += calculateGroupAvgMark(groups[i]);
+            for (int j = 0; j < groups[i].length; j++) {
+                sum += groups[i][j];
+            }
+            count += groups[i].length;
+
         }
-        return sum / groups.length;
+        return sum / count;
     }
 
-    private static double calculateGroupAvgMark(int[] group) {
-        double sum = 0;
-        for (int i = 0; i < group.length; i++) {
-            sum += group[i];
+    public static String FindBadStudentGroups(int[][] groups) {
+        String result = "";
+        for (int i = 0; i < groups.length; i++) {
+            for (int j = 0; j < groups[i].length; j++) {
+                if (groups[i][j] < 4) {
+                    result += (i + 1) + " ";
+                    break;
+                }
 
+            }
         }
-        return sum / group.length;
+        return result.trim();
+    }
+
+    public static String FindGroupsWithoutBadStudent(int[][] groups) {
+        String result = "";
+        for (int i = 0; i < groups.length; i++) {
+
+            boolean flag = true;
+            for (int j = 0; j < groups[i].length; j++) {
+                if (groups[i][j] < 4) {
+                    flag = false;
+                    break;
+                }
+
+            }
+
+            if (flag) {
+
+                result += (i + 1) + " ";
+            }
+        }
+        return result.trim();
     }
 }
-
+////    private static double calculateGroupAvgMark(int[] group) {
+////        double sum = 0;
+////        for (int i = 0; i < group.length; i++) {
+////            sum += group[i];
+////
+////        }
+//        return sum / group.length;
+//    }
+//}
+//
--- lesson21/SchoolProject/test/by/itstep/klimenko/lesson22/model/logic/SchoolManagerTest.java
+++ lesson21/SchoolProject/test/by/itstep/klimenko/lesson22/model/logic/SchoolManagerTest.java
@@ -6,21 +6,58 @@ import org.junit.Test;
 
 
 public class SchoolManagerTest {
+
+    private int[][] getGroups() {
+
+        int[][] groups =
+                {
+                        {7, 7, 8, 2},         // 4.0
+                        {9, 8, 9, 10},        // 9.0
+                        {7, 7, 1},
+                        {8, 8, 10, 10}                  // 5.0
+                };
+
+
+        return groups;
+
+    }
+
+
     @Test
 
     public void testCalculateAvgMarkPositive() {
-        int[][] groups =
-                {
-                        {7, 8, 9},         // 8.0
-                        {9, 8, 9, 10},     // 9.0
-                        {7, 7}             // 7.0
-                };
-        double expected = 8.0;
+        int[][] groups = getGroups();
+        double expected = 7.5;
 
         double actual = SchoolManager.CalculateAvgMark(groups);
 
-        assertEquals(expected, actual, 0.01);
+        assertEquals(expected, actual, 0.5);
 
     }
+
+    @Test
+    public void testFindBadStudentGroups() {
+        int[][] groups = getGroups();
+        String expected = "1 3";
+
+        String actual = SchoolManager.FindBadStudentGroups(groups);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testFindGroupsWithoutBadStudentsPositive() {
+        int[][] groups = getGroups();
+        String expected = "1 3";
+
+        String actual = SchoolManager.FindBadStudentGroups(groups);
+
+        assertEquals(expected, actual);
+
+
+    }
+
+
 }
 
